c0d488c62bcb3b7fabe18a1788e655d9637e89e6
VuceticBranislav <vucetic985@gmail.com>
2021-03-14 20:26:21 +0100

    Sprint2 me (#22)
    * Sprint 2#
    
    Dodao funkciju za popunjavanje polaznih podataka.
    Zamenio upis podataka ovom novom funkcijom. Privremeno.
    Popravio zamenjeno off i h.
    
    * Sprint 2# Part 2
    
    Dodat ispis detaljnih rezultata proracuna.

--- src/liftingLug/HECalcProcedural.java
+++ src/liftingLug/HECalcProcedural.java
@@ -22,7 +22,6 @@ package liftingLug;/* Glavni kostur pograma
 public class HECalcProcedural {
     static double W;
     static String lugMat;
-    static String vessMat;
     static double YIELD;
     static double w;
     static double t;
@@ -45,7 +44,7 @@ public class HECalcProcedural {
 
     static public void main(String[] args) {
         func_introduction();
-        func_inputEcho();
+        func_dummyInput(); // func_inputEcho();
         func_showInputs();
         func_calculate();
         func_report();
@@ -58,6 +57,28 @@ public class HECalcProcedural {
         System.out.println("*****************************************************************************************");
         System.out.println("");
     }
+    
+    /**
+     * Test function.
+     * Placed here for test purposes. Remove in later builds.
+     * 
+     * @author Me
+     */
+    static void func_dummyInput() {
+      W = 6500.0;
+      lugMat = "SA/EN 10028-2";
+      YIELD = 244.0;
+      w = 120.0;
+      t = 15.0;
+      dh = 40.0;
+      r = 40.0;
+      h = 60.;        
+      off = 80.0;
+      fax = 165.0;
+      fn = 0.0;
+      ft = 3250.0;
+      impfac = 2.0;
+  }
 
     static void func_inputEcho() {
         System.out.println("Let's do inputs!\n");
@@ -67,9 +88,6 @@ public class HECalcProcedural {
         System.out.println("Input lifting lug material. (This is for information only!)");
         System.out.print("\tLifting lug material:");
         lugMat = TextIO.getlnString();
-        System.out.println("Input vessel material. (This is for information only!)");
-        System.out.print("\tVessel material:");
-        vessMat = TextIO.getlnString();
         System.out.println("Input yield value for lifting lug material YIELD[MPa].");
         System.out.print("\tYIELD[MPa]:");
         YIELD = TextIO.getlnDouble();
@@ -85,10 +103,10 @@ public class HECalcProcedural {
         System.out.println("Input radius of semi-circular arc of lifting lug r[mm]. Less than w/2");
         System.out.print("\tr[mm]:");
         r = TextIO.getlnDouble();
-        System.out.println("Input height of lifting lug from bottom to center of hole h[mm].");
+        System.out.println("Input offset from vessel OD to center of hole h[mm].");
         System.out.print("\th[mm]:");
-        h = TextIO.getlnDouble();
-        System.out.println("Input offset from vessel OD to center of hole off[mm].");
+        h = TextIO.getlnDouble();        
+        System.out.println("Input height of lifting lug from bottom to center of hole off[mm].");
         System.out.print("\toff[mm]:");
         off = TextIO.getlnDouble();
         System.out.println("Input force along vessel axis fax[N].");
@@ -112,7 +130,6 @@ public class HECalcProcedural {
         System.out.println("");
         System.out.println("Vessel weight W:                                         " + W + "[N]");
         System.out.println("Lifting lug material:                                    " + lugMat);
-        System.out.println("Vessel material:                                         " + vessMat);
         System.out.println("YIELD:                                                   " + YIELD + "[MPa]");
         System.out.println("Lifting lug maximum width w:                             " + w + "[mm]");
         System.out.println("Lifting lug uniform thickness t:                         " + t + "[mm]");
@@ -126,8 +143,12 @@ public class HECalcProcedural {
         System.out.println("Impact factor:                                           " + impfac);
         System.out.println("*****************************************************************************************");
     }
-
+    
     static void func_calculate() {
+        System.out.println("Applying the Impact factor to the loads:");
+        System.out.printf("  Fax = %.2f * Impfac = %.2f N\n", fax, fax * impfac);
+        System.out.printf("  Ft  = %.2f * Impfac = %.2f N\n", ft, ft * impfac);
+        System.out.printf("  Fn  = %.2f * Impfac = %.2f N\n\n", fn, fn * impfac);
         fax = fax * impfac;
         ft = ft * impfac;
         fn = fn * impfac;
@@ -144,40 +165,77 @@ public class HECalcProcedural {
     static void func_calculateShearStress() {
         // Shear stress in lug above hole
         shs = Math.sqrt(Math.pow(fax, 2) + Math.pow(fn, 2) + Math.pow(ft, 2)) / (2 * (r - dh / 2)*t);
+        
+        System.out.println("Shear Stress in Lug above Hole [Shs]:");
+        System.out.println("  = Sqrt( Fax^2 + Fn^2 + Ft^2 ) / (2*(r-dh/2))*t");
+        System.out.printf("  = Sqrt( %.0f^2 + %.0f^2 + %.0f^2 ) / %.3f\n", fax, fn, ft, 2 * (r - dh / 2)*t);
+        System.out.printf("  = %.2f MPa\n\n", shs);
     }
 
     static void func_calculateAllowableShearStress() {
         // Allowable shear stress in lug above hole
         sas = 0.4 * YIELD;
+        
+        System.out.println("Allowable Shear Stress in Lug above Hole [Sas]:");
+        System.out.println("  = 0.4 * Yield");
+        System.out.printf("  = 0.4 * %.0f\n", YIELD);
+        System.out.printf("  = %.2f MPa\n\n", sas);
     }
 
     static void func_calculatePinHoleStress() {
         // Pin hole bearing stress
         pbs = Math.sqrt(Math.pow(fax, 2) + Math.pow(fn, 2)) / (t * dh);
+        
+        System.out.println("Pin Hole Bearing Stress [Pbs]:");
+        System.out.println("  = Sqrt( Fax^2 + Fn^2 ) / ( t * dh )");
+        System.out.printf("  = Sqrt( %.0f^2 + %.0f^2 ) / ( %.3f * %.3f )\n",  fax, fn, t, dh);
+        System.out.printf("  = %.2f MPa\n\n", pbs);
     }
 
     static void func_calculateAllowablePinHoleStress() {
         // Allowable bearing stress
         pba = 0.75 * YIELD;
+        
+        System.out.println("Allowable Bearing Stress [Pba]:");
+        System.out.println("  = 0.75 * Yield");
+        System.out.printf("  = 0.75 * %.0f\n", YIELD);
+        System.out.printf("  = %.2f MPa\n\n", pba);
     }
 
     static void func_calculateBendingStress() {
         // Bending stress at the base of the lug
         fbs = ft * off / (w * Math.pow(t, 2) / 6) + fax * off / (Math.pow(w, 2) * t / 6);
+        
+        System.out.println("Bending stress at the base of the lug [Fbs]:");
+        System.out.println("  = Ft*off / (w*t^2 / 6) + Fax*off / (w^2*t / 6)");
+        System.out.printf("  = %.0f * %.3f / (%.3f * %.3f^2 / 6) +\n",  ft, off, w, t);
+        System.out.printf("    %.0f * %.3f / (%.3f^2 * %.3f / 6)\n",  fax, off, w, t);
+        System.out.printf("  = %.2f MPa\n\n", fbs);         
     }
 
     static void func_calculateTensileStress() {
         // Tensile stress at the base of the lug
         fa = fn / (w * t);
+        
+        System.out.println("Tensile stress at the base of the lug [Fa]:");
+        System.out.printf("  = Fn / (w * t) = %.0f / (%.3f * %.3f )\n",  fn, w, t);
+        System.out.printf("  = %.2f MPa\n\n", fa);  
     }
 
     static void func_calculateTotalCombinedStress() {
         fcom = fbs + fa;
+        
+        System.out.println("Total Combined Stress at the base of the lug:");
+        System.out.printf("  = Fbs + Fa = %.2f MPa\n\n", fcom);  
     }
 
     static void func_calculateLugAllowableStress() {
         // Allowable lug stress for bending and tension
         lall = 0.66 * YIELD;
+        
+        System.out.println("Lug Allowable Stress for Bending and Tension:");
+        System.out.println("  = 0.66 * Yield");
+        System.out.printf("  = 0.66 * %.0f = %.2f MPa\n", YIELD, lall);
     }
 
     static void func_report() {
